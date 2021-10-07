63769cbc8adc2689b160f1253d20fd881be94d63
Vendra <vendra0408@gmail.com>
2021-10-07 20:15:14 +0530

    Initial commit

---
+++ src/DeclaringVariables.java
@@ -0,0 +1,17 @@
+//Declare Variable int, float, double, char, boolean and assign it a value.
+public class DeclaringVariables {
+    public static void main(String args[])
+    {
+       int int_number = 1000;
+       float float_number = 10.292f;
+       double double_number = 0.0000000000512365123;
+       char char_name = 'N';
+       boolean bool_accept = true;
+
+        System.out.println("Value of Int is:\n" +int_number);
+        System.out.println("Value of Float is:\n" +float_number);
+        System.out.println("Value of Double is:\n" +double_number);
+        System.out.println("Value of Char is:\n" +char_name);
+        System.out.println("Value of Bool is:\n" +bool_accept);
+    }
+}
