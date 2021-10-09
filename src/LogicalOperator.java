28020db0e19d90a2472781be92d268b57071539e
Vendra <vendra0408@gmail.com>
2021-10-09 19:47:47 +0530

    Initial Commit

---
+++ src/LogicalOperator.java
@@ -0,0 +1,14 @@
+public class LogicalOperator {
+    public static void main(String args[]){
+        boolean x = true;
+        boolean y = true;
+        boolean answer;
+
+        boolean not_x = !x;
+        boolean xor_x = not_x ^ x;
+        boolean and_xy = xor_x && y;
+        answer = !and_xy;
+
+        System.out.println(answer);
+    }
+}
