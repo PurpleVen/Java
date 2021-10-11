5261005118042a98543c1e6588e9edf6f83c7d2a
Vendra <vendra0408@gmail.com>
2021-10-11 23:29:39 +0530

    Initial Commit

---
+++ src/ComparingStrings.java
@@ -0,0 +1,12 @@
+public class ComparingStrings {
+    public static void main(String args[]){
+        String one = "Hello";
+        String two = "World";
+        String three = "hello";
+        String four = "Hello";
+
+        System.out.println(one.equals(two));
+        System.out.println(one.equals(three));
+        System.out.println(one.equals(four));
+    }
+}
