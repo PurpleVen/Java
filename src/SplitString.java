f8ff77f5f667afe9e14057edc05b7b59fe9ca546
Vendra <vendra0408@gmail.com>
2021-10-11 22:16:03 +0530

    Initial Commit

---
+++ src/SplitString.java
@@ -0,0 +1,9 @@
+public class SplitString {
+    public static void main(String args[]){
+       String Split="Hello World,Java,Split String";
+       String[] Spliting = Split.split(",");
+        System.out.println(Spliting[0]);
+        System.out.println(Spliting[1]);
+        System.out.println(Spliting[2]);
+    }
+}
