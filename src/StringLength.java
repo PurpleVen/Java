57cb1ca5b972c5d0cdf8c2545d1a39be5976bb30
Vendra <vendra0408@gmail.com>
2021-10-11 23:33:06 +0530

    Initial Commit

---
+++ src/StringLength.java
@@ -0,0 +1,6 @@
+public class StringLength {
+    public static void main(String args[]){
+        String length = "Hello World";
+        System.out.println("The length of string is: "+length.length());
+    }
+}
