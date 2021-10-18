4e639917e716260af2e8c788bc86629dfabf1925
Vendra <vendra0408@gmail.com>
2021-10-18 10:47:42 +0530

    Initial Commit

---
+++ src/PrimitiveType.java
@@ -0,0 +1,14 @@
+
+
+public class PrimitiveType {
+    public static void main(String args[]){
+        String name = "Lisa";
+        changeName(name);
+        System.out.println(name);
+    }
+
+public static void changeName(String n){
+    n = "Mona";
+    System.out.println(n);
+    }
+}
