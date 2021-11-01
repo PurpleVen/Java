73657f6a9b433d06f75b860737953a4e11980803
Vendra <vendra0408@gmail.com>
2021-11-01 14:31:08 +0530

    Initial Commit

---
+++ src/Array.java
@@ -0,0 +1,18 @@
+public class Array {
+    public static void main(String args[]){
+        int[] Arr = new int[5];
+        System.out.println("The Size Of Array is: "+Arr.length);
+
+        Arr[0] = 1;
+        Arr[1] = 2;
+        Arr[2] = 3;
+        Arr[3] = 4;
+        Arr[4] = 5;
+
+        System.out.println("First element:  "+Arr[0]);
+        System.out.println("Second element: "+Arr[1]);
+        System.out.println("Third element:  "+Arr[2]);
+        System.out.println("Fourth element: "+Arr[3]);
+        System.out.println("Fifth element:  "+Arr[4]);
+    }
+}
