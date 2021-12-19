1712fce0d6afd3df8e6aabdc1980374bc37d84a2
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2021-12-19 21:50:10 +0530

    Add files via upload

---
+++ src/ArrayMain.java
@@ -0,0 +1,13 @@
+package com.example.jbl_3356.WrapperClassandArrayList;
+
+import java.util.ArrayList;
+public class ArrayMain {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
