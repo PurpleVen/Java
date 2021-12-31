629cdd07c0e91ecd14963bad22c3ada05c7e7e01
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2021-12-31 11:16:31 +0530

    Add files via upload

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World !!");
+    }
+}
+
+
+
+//Make project
+//create class
+//Make main method
+// print something from it
