5c15ed931708dc0f126dfeedb587a6dbc3aa884a
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-07-13 23:43:04 +0530

    Add files via upload

---
+++ src/IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse
+{
+    public static void main(String[] args)
+    {
+        int marks=10;
+        if(marks==20)
+            System.out.println("You got distinction");
+        if(marks==8)
+            System.out.println("You got D grade");
+        else
+            System.out.println("You didn't attend the exam");
+    }
+}
