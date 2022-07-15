5bcb3b4c963744691b15a79ddb672f7838bb4e13
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-07-15 20:17:41 +0530

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String args[]){
+        Stack s1,s2;
+        s1 = new Stack();
+        s2 = new Stack();
+        s1.push(num:3);
+        s2.push(num:6);
+
+        System.out.println("Stack 1"+s1.pop());
+        System.out.println("Stack 2"+s2.pop());
+    }
+}
+
+class College;
+class Department child class of College
+class teachers; child Department
