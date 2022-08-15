f5cdbaa852752598b3ced4a8427a60131d271069
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-08-15 15:19:06 +0530

    Add files via upload

---
+++ src/Loop.java
@@ -0,0 +1,12 @@
+public class Loop
+{
+    public static void main(String[] args)
+    {
+        int x ;
+        for(x=0;x<10;x++)
+        {
+            System.out.println("X = "+x);
+        }
+
+    }
+}
